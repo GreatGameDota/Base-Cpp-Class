4c6ce329bb5fb848515ec0bad1895643f29aff7c
GreatGameDota <17858650+GreatGameDota@users.noreply.github.com>
2019-08-13 10:14:37 -0500

    Reformat and rename helpers folder

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include "Class.h"
-#include "./helpers/TimeElapsed.h"
+#include "./util/TimeElapsed.h"
 #include <iostream>
 
 using namespace std;
@@ -10,25 +10,19 @@ int main()
 
   Class c{10, 2};
   Class c2{c};
-  cout << c.getData() << endl;
-  cout << c.getNum() << endl;
-  cout << "c2:\n"
-       << c2.getData() << endl;
-  cout << c2.getNum() << endl;
+  cout << c.getData() << " " << c.getNum() << endl;
+  cout << "c2: " << c2.getData() << " " << c2.getNum() << endl;
 
   cout << c.getStr() << endl;
   c.setStr("test");
   cout << c2.getStr() << endl;
 
   Class c3;
-  cout << c3.getData() << endl;
-  cout << c3.getNum() << endl;
+  cout << c3.getData() << " " << c3.getNum() << endl;
   c3 = c;
-  cout << c3.getData() << endl;
-  cout << c3.getNum() << endl;
+  cout << c3.getData() << " " << c3.getNum() << endl;
   c3 = Class{2, 10};
-  cout << c3.getData() << endl;
-  cout << c3.getNum() << endl;
+  cout << c3.getData() << " " << c3.getNum() << endl;
 
   cout << "Change Num of c3: ";
   cin >> c3;
